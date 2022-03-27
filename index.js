a793a5b9adc02c3644e4044be0324d4898cbc35e
Destin Moulton <destin.moulton@gmail.com>
2022-03-27 16:00:20 -0600

    Added glob

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 require("dotenv").config();
+const glob = require("glob");
+
+(async () => {
+    const longpath = process.env.VAULT_PATH + "/**/*.md";
+    glob(longpath, {}, function (err, files) {
+        if (err) throw err;
+
+        console.log(files);
+    });
+})();
