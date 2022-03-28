ae5ca74eb6f9ee16f8fa12896319b4b10221a12a
Destin Moulton <destin.moulton@gmail.com>
2022-03-28 16:04:40 -0600

    Added regex to exclude dotfiles

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ require("dotenv").config();
 const dirTree = require("directory-tree");
 const express = require("express");
 const app = express();
+const nunjucks = require("nunjucks");
+const path = require("path");
+const slugify = require("slugify");
+
+//app.set("views", "./public");
+nunjucks.configure(path.resolve(__dirname, "public", "views"), {
+    autoescape: true,
+    express: app,
+});
 
 const options = {
     extensions: /\.md/,
     attributes: ["type"],
+    exclude: /.*\/\..*/,
 };
-const tree = dirTree(process.env.VAULT_PATH, options);
-
-app.get("/", function (req, res) {
-    res.send("hello");
+const slugger = (item, path) => {
+    // Strip the file extension
+    const noExtension = item.name.replace(/\.[^/.]+$/, "");
+    item.slug = slugify(noExtension);
+};
+const tree = dirTree(process.env.VAULT_PATH, options, slugger, slugger);
+console.log(tree.children[0]);
+app.get("/*", function (req, res) {
+    console.log(req.url);
+    res.render("index.html", { tree: tree });
 });
-console.log(`Listening on port ${process.env.PORT}`);
-app.listen(process.env.PORT);
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+});
